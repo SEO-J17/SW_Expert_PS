7d3c9e48e108e677bc1d6b1742b8c3b8bd044730
SEO-J17 <qw4643987@naver.com>
2021-09-01 22:58:57 +0900

    09-01

--- src/D1/홀수만더하기_2072.java
+++ src/D1/홀수만더하기_2072.java
@@ -1,10 +1,30 @@
 package D1;
 
-public class 홀수만더하기_2072 {
+import java.io.*;
+import java.util.*;
 
+public class 홀수만더하기_2072 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int t;
+		t = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= t; i++) {
+			int temp = 0;
+			int sum = 0;
+			for (int j = 0; j < 10; j++) {
+				temp = sc.nextInt();
+				if (temp % 2 != 0) {
+					sum += temp;
+				}
+			}
+			
+			sb.append("#" + i + " ").append(sum).append("\n");
+			
+		}
+
+		System.out.print(sb);
 
 	}
-
 }
