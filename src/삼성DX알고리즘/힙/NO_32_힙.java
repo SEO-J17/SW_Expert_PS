7340fb6248011be1368f6f58afd2d257d2d38705
SEO-J17 <qw4643987@naver.com>
2022-07-28 18:57:01 +0900

    solved 07-28

---
+++ src/삼성DX알고리즘/힙/NO_32_힙.java
@@ -0,0 +1,41 @@
+package 伙己DX舅绊府硫.赛;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class NO_32_赛 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int tc = Integer.parseInt(br.readLine());
+		for (int l = 1; l <= tc; l++) {
+			sb.append("#" + l + " ");
+			int n = Integer.parseInt(br.readLine());
+			PriorityQueue<Integer> qu = new PriorityQueue<>(Collections.reverseOrder());
+			
+			for (int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int cmd = Integer.parseInt(st.nextToken());
+				if (cmd == 1) {
+					qu.add(Integer.parseInt(st.nextToken()));
+				} else if (cmd == 2) {
+					if (!qu.isEmpty()) {
+						sb.append(qu.poll() + " ");
+					} else {
+						sb.append(-1).append(" ");
+					}
+				}
+			}
+			
+			sb.append("\n");
+			
+		}
+
+		System.out.println(sb);
+
+	}
+}
